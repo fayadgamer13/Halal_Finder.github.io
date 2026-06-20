f5afb5309d579ebe6a0ed14a7e14b2f80320a211
fayadgamer13 <wadifayad@gmail.com>
2026-06-20 20:09:53 +1000

    Add download functionality for Windows installer

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ document.addEventListener('DOMContentLoaded', () => {
         window.location.href = apkUrl;
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const downloadBtn = document.getElementById('downloadwindowsBtn');
+
+    downloadBtn.addEventListener('click', () => {
+        // Replace with your actual APK download link
+        const apkUrl = 'https://github.com/fayadgamer13/Halal_Finder/releases/download/1.4.0w/east_halalscan_installer.exe';
+        
+        // Simple feedback for the user
+        console.log("Starting download...");
+        
+        // Trigger the download
+        window.location.href = apkUrl;
+    });
+});
